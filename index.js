0032ecaa0ff5ab82a4e60d6ba33f389c6b608406
Yekta <omeryekta@sabanciuniv.edu>
2024-06-02 22:20:59 +0300

    correcting order of routes in index

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,  'index.html'));
-});
 app.use("/api/configs", configRoutes);
+const PORT = process.env.PORT || 3000;
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
